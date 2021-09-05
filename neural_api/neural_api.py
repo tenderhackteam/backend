20a2c7b45f4077e1b7e6de161d07179a0ff9beb0
fodro <feodor.tomilov@yandex.ru>
2021-09-05 17:15:21 +0500

    poop

--- neural_api/neural_api.py
+++ neural_api/neural_api.py
@@ -52,13 +52,14 @@ async def neural_generate(user: UserFromNeuralApi, top_n: int):
 async def neural_generate(top_n: int, item_id: int):
     neural_body = json.dumps(
         {
-            "seen": app.seen_basket[user.user_id] + app.compare_basket[user.user_id] + app.cart_basket[user.user_id],
+            "item_id": item_id,
             "top_n": top_n,
         }
     )
     if not app.redis_client.exists(neural_body):
         neural_rpc = await NeuralRpcClient(app.event_loop).connect()
         response = await neural_rpc.call(neural_body)
+        app.redis_client.set(neural_body)
         return response
     return app.redis_client.get(neural_body)
 
